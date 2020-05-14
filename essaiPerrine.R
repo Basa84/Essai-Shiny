b1b4407f24a947d3a726e886a425dd50b436b500
Perrine98 <perrine.truche@free.fr>
2020-05-14 10:33:19 +0200

    Update essaiPerrine.R

--- essaiPerrine.R
+++ essaiPerrine.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library(shiny)
 
 ui <- fluidPage(
@@ -22,4 +23,30 @@ server <- function(input, output) {
   
 }
   
+=======
+library(shiny)
+
+ui <- fluidPage(
+    titlePanel("Application de Perrine"),
+    textInput("name", "Iinqiquez votre nom :"),
+    textOutput("bienvenue"),
+    actionButton("go", "visualisez!"),
+    img(src="<Strava.png>")
+)
+
+server <- function(input, output) {
+  output$bienvenue <- renderText({
+  re <- eventReactive(
+      input$go,{input$name})
+   
+      re()
+  })
+  output$image <- renderPrint({
+    dataset <- get(input$img, "Strava.png")
+ 
+  })
+  
+}
+  
+>>>>>>> f859a77003a15bcc29ee97e403f0380242d3dccb
 shinyApp(ui = ui, server = server)
